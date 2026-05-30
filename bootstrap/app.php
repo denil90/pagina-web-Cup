d850a013e484acfd379fa6fe96332b7e7cdd48a5
Denilson Cruz <newbeekool@gmail.com>
2026-05-29 22:20:40 -0400

    arreglando bug de la vista en Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'verificar.rol' => \App\Http\Middleware\VerificarRol::class,
         ]);
